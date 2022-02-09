a240d2c9051b35abd8b2e577e304652cb1691de4
hannescreater <80467379+hannescreater@users.noreply.github.com>
2022-02-09 16:34:25 +0100

    Add files via upload

--- dataForGUI.java
+++ dataForGUI.java
@@ -1,5 +1,10 @@
-public class dataForGUI{
-  public dataForGUI(int storedAlcoholics){
-
+public class dataForGUI{                                                                //Makes Data easier to access for use in the GUI
+  int storedAlcs;                                                                       //Renames every variable to make it shorter/faster to access
+  int maxCap;
+  int peopDrink;
+  public dataForGUI(int storedAlcoholics, int maximalCapacity, int peopleDrinking){
+    storedAlcs    = storedAlcoholics;
+    maxCap        = maximalCapacity;
+    peopDrink     = peopleDrinking;
   }
 }
--- userInputsAtStart.java
+++ userInputsAtStart.java
@@ -1,9 +1,13 @@
 import javax.swing.JOptionPane;
 
 public class userInputsAtStart{
-  pulic int userInputs(int maximalCapacity, int storedAlcoholics);
-    userInputs[0] = JOptionPane.showInputDialog("How many people do you want do be drinking?");
-    userInputs[1] = JOptionPane.showInputDialog("How much beer do you want to be available at start?");
-    return
+  pulic int userInputs(int maximalCapacity, int storedAlcoholics, int peopleDrinking){
+    JOptionPane.showMessageDialog(null, "Hello there, this game is basically about drinking alcohol until you're dead! Do you want to play a round? Let's go!");
+    maximalCapacity   = JOptionPane.showInputDialog("How much beer do you want to be maximally storable?");
+    peopleDrinking    = JOptionPane.showInputDialog("How many people do you want do be drinking?");
+    storedAlcoholics  = JOptionPane.showInputDialog("How much beer do you want to be available at start?");
+    return maximalCapacity;
+    return peopleDrinking;
+    return storedAlcoholics;
   }
 }
